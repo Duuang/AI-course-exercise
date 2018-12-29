ac8d4a91901e0816ae923b236c0b893276d3d7b0
cky <15201692553@126.com>
2018-12-30 07:32:32 +0800

    v2.0 参数优化，修正错误，完善注释

--- 蚁群算法ACO/main.cpp
+++ 蚁群算法ACO/main.cpp
@@ -7,46 +7,57 @@
 #include <string>
 
 //**************************************
+//
+//author: Duuang
+//https://github.com/Duuang/AI-course-exercise
+//
+//**************************************
+
+
+//**************************************
 //parameters
 //**************************************
 
-#define RANDOM_RANGE 100000
+#define RANDOM_RANGE 100000000  //生成随机数的最大范围，可以不用管
 
-#define PHEROMONE_INIT_VALUE 100
+#define PHEROMONE_INIT_VALUE 1.0  //城市间初始化的信息素浓度，影响不大
 
-#define NUM_OF_CITIES 24
-#define POPULATION 50
-#define INFLUENCE_OF_PHEROMONE 1
-#define INFLUENCE_OF_DISTANCE 10
-#define VOLATILIZATION 0.01
+#define NUM_OF_CITIES 48  //城市个数
+#define POPULATION 34     //蚂蚁个数，m值
+#define INFLUENCE_OF_PHEROMONE 1  //α值，信息素的影响权重
+#define INFLUENCE_OF_DISTANCE 5   //β值，当前距离的影响权重
+#define Q_VALUE 100     //Q值，每次蚂蚁走完一圈之后，一共释放的信息素的量
+#define VOLATILIZATION 0.5  //(1 - ρ）中 ρ 的值，挥发系数
 
-#define MAX_ITERATION_TIMES 1000000
+#define MAX_ITERATION_TIMES 1000000  //最大迭代次数
 
+//城市坐标
 typedef struct {
   int x, y;
 }City;
 
+//蚂蚁个体
 typedef struct {
-  int route[NUM_OF_CITIES];
-  int visited[NUM_OF_CITIES];
-  int currentsteps;
+  int route[NUM_OF_CITIES];  //路径记录
+  int visited[NUM_OF_CITIES];  //已访问记录
+  int currentsteps;  //现在已经走的步数
 
-  double distance_sum;
+  double distance_sum;  //个体走的总距离
 }TSP;
 
 //**************************************
 //global variables start here
-//**************************************
-City cities[NUM_OF_CITIES] = { {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6}, {1, 7}
-, {2, 7}, {3, 7}, {4, 7}, {5, 7}, {6, 7}, {7, 7}, {7, 6}, {7, 5}, {7, 4}, {7, 3}, {7, 2}, {7, 1}
-, {6, 1}, {5, 1}, {4, 1}, {3, 1}, {2, 1} };
+//**************************************、
+City cities[NUM_OF_CITIES];  //城市
 
-double distance[NUM_OF_CITIES][NUM_OF_CITIES];
-double pheromone[NUM_OF_CITIES][NUM_OF_CITIES];
+double distance[NUM_OF_CITIES][NUM_OF_CITIES];  //城市间距离
+double pheromone[NUM_OF_CITIES][NUM_OF_CITIES];  //城市间信息素浓度
 
-TSP ant[POPULATION];
+TSP ant[POPULATION];  //蚁群
 
-
+//
+//生成[x,y]范围的随机整数
+//
 int Random(int x, int y) {
   std::random_device rd;
   //std::default_random_engine a;
@@ -55,6 +66,9 @@ int Random(int x, int y) {
   return dist(rng);
 }
 
+//
+//初始化距离矩阵和蚂蚁位置
+//
 void Init() {
   //初始化距离矩阵，下标从0开始，到n-1
   for (int i = 0; i < NUM_OF_CITIES; i++) {
@@ -77,11 +91,14 @@ void Init() {
     ant[i].currentsteps = 0;
     ant[i].distance_sum = 0;
   }
-
 }
 
-void AllAntsMoveOneStep() {
+//
+//所有蚂蚁走完一圈完整的路径
+//
+void AllAntsMoveAround() {
   for (int i = 0; i < POPULATION; i++) {
+    //每个蚂蚁的每步：当步数足够时跳出循环
     while (ant[i].currentsteps < NUM_OF_CITIES - 1) {
       int current_city = ant[i].route[ant[i].currentsteps];
       double decision_sum = 0;
@@ -91,51 +108,97 @@ void AllAntsMoveOneStep() {
         if (ant[i].visited[j]) {
           continue;
         }
+        //当前路径概率
         decision_calculation[j] = pow(pheromone[current_city][j], INFLUENCE_OF_PHEROMONE)
           * pow(1 / distance[current_city][j], INFLUENCE_OF_DISTANCE);
+        //概率的和
         decision_sum += decision_calculation[j];
       }
-      //按概率走一步 ？ 还是按最大的走（不能吧）？
-      int random = Random(0, RANDOM_RANGE - 1);  //TODO: 减了1，怕万一算完了正好卡在这个值上。。不过几率很小
-      int lastcity = -1;
-      double lower_bound[NUM_OF_CITIES] = { 0 };
-      double upper_bound[NUM_OF_CITIES] = { 0 };
-      bool firsttime = true;
-      //给每个城市分配随机数中奖区间
-      for (int j = 0; j < NUM_OF_CITIES; j++) {
-        if (ant[i].visited[j]) {
-          continue;
-        }
-        if (firsttime) {
-          lower_bound[j] = 0;
-          upper_bound[j] = lower_bound[j] + (decision_calculation[j] / decision_sum) * RANDOM_RANGE;
-          firsttime = false;
-          lastcity = j;
-        } else {
-          lower_bound[j] = upper_bound[lastcity];
-          upper_bound[j] = lower_bound[j] + (decision_calculation[j] / decision_sum) * RANDOM_RANGE;
-          lastcity = j;
-        }
-      }
-      //看哪个城市中奖，然后走
-      for (int j = 0; j < NUM_OF_CITIES; j++) {
-        if (ant[i].visited[j]) {
-          continue;
-        }
-        if (lower_bound[j] <= random && upper_bound[j] > random) {
-          ant[i].route[ant[i].currentsteps + 1] = j;
-          ant[i].currentsteps++;
-          ant[i].visited[j] = 1;
-          break;
+      //按转移概率，用轮盘赌方式，选择下一步
+      double random = Random(0, RANDOM_RANGE);
+      //---------------------更改的部分，其实底下注释掉的部分也是一样，也没有错误：
+      bool ifcanselect = false;
+      int lastj = -100;
+      if (decision_sum > 0) {  //当迭代次数很大时，decision_sum甚至都会变成0，这时不单独处理的话程序就挂掉了
+        random = random * (decision_sum / RANDOM_RANGE);
+        for (int j = 0; j < NUM_OF_CITIES; j++) {
+          if (ant[i].visited[j]) {
+            continue;
+          } else {
+            random = random - decision_calculation[j];
+            if (random < 0.0) {
+              ant[i].route[ant[i].currentsteps + 1] = j;
+              ant[i].currentsteps++;
+              ant[i].visited[j] = 1;
+              ifcanselect = true;
+              break;
+            }
+            lastj = j;
+          }
         }
       }
+      if (!ifcanselect) {  //如果decision_sum就是0，无法选择的情况，则选第一个能走的，直接走
+        for (int j = 0; j < NUM_OF_CITIES; j++) {
+          if (ant[i].visited[j] == 0) {
+            ant[i].route[ant[i].currentsteps + 1] = j;
+            ant[i].currentsteps++;
+            ant[i].visited[j] = 1;
+          }
+        }
+      }
+      //int lastcity = -1;
+      //double lower_bound[NUM_OF_CITIES] = { 0 };
+      //double upper_bound[NUM_OF_CITIES] = { 0 };
+      //bool firsttime = true;
+      ////给每个城市分配随机数中奖区间
+      //for (int j = 0; j < NUM_OF_CITIES; j++) {
+      //  if (ant[i].visited[j]) {
+      //    continue;
+      //  }
+      //  if (firsttime) {
+      //    lower_bound[j] = 0;
+      //    upper_bound[j] = lower_bound[j] + (decision_calculation[j] / decision_sum) * RANDOM_RANGE;
+      //    firsttime = false;
+      //    lastcity = j;
+      //  } else {
+      //    lower_bound[j] = upper_bound[lastcity];
+      //    upper_bound[j] = lower_bound[j] + (decision_calculation[j] / decision_sum) * RANDOM_RANGE;
+      //    lastcity = j;
+      //  }
+      //}
+      ////看哪个城市中奖，然后走
+      //int lastj = -10;
+      //bool ifcanselect = false;
+      //for (int j = 0; j < NUM_OF_CITIES; j++) {
+      //  if (ant[i].visited[j]) {
+      //    continue;
+      //  }
+      //  if (lower_bound[j] <= random && upper_bound[j] > random) {
+      //    ant[i].route[ant[i].currentsteps + 1] = j;
+      //    ant[i].currentsteps++;
+      //    ant[i].visited[j] = 1;
+      //    ifcanselect = true;
+      //    break;
+      //  }
+      //  lastj = j;
+      //}
+      //if (!ifcanselect) {
+      //  ant[i].route[ant[i].currentsteps + 1] = lastj;
+      //  ant[i].currentsteps++;
+      //  ant[i].visited[lastj] = 1;
+
+      //}
     }
   }
 }
 
-void UpdatePheromoneAndInit() {
-  //更新信息素
-  
+//
+//更新信息素
+//
+void UpdatePheromone() {
+  //存放蚂蚁这圈，在城市间增加的信息素
+  double tmp_pheromone[NUM_OF_CITIES][NUM_OF_CITIES];
+  memset(tmp_pheromone, 0, sizeof(tmp_pheromone));
   for (int i = 0; i < POPULATION; i++) {
     //计算总距离
     for (int j = 0; j < NUM_OF_CITIES - 1; j++) {
@@ -144,50 +207,103 @@ void UpdatePheromoneAndInit() {
     ant[i].distance_sum += distance[ant[i].route[0]][ant[i].route[NUM_OF_CITIES - 1]];
     //根据总距离更新信息素
     for (int j = 0; j < NUM_OF_CITIES - 1; j++) {
-      pheromone[ant[i].route[j]][ant[i].route[j + 1]] = (1 - VOLATILIZATION) * pheromone[ant[i].route[j]][ant[i].route[j + 1]];
+      tmp_pheromone[ant[i].route[j]][ant[i].route[j + 1]] += Q_VALUE / ant[i].distance_sum;
+      tmp_pheromone[ant[i].route[j + 1]][ant[i].route[j]] += Q_VALUE / ant[i].distance_sum;
+      /*pheromone[ant[i].route[j]][ant[i].route[j + 1]] = (1 - VOLATILIZATION) * pheromone[ant[i].route[j]][ant[i].route[j + 1]];
       pheromone[ant[i].route[j + 1]][ant[i].route[j]] = (1 - VOLATILIZATION) * pheromone[ant[i].route[j + 1]][ant[i].route[j]];
-      pheromone[ant[i].route[j]][ant[i].route[j + 1]] += 100 / ant[i].distance_sum;
-      pheromone[ant[i].route[j + 1]][ant[i].route[j]] += 100 / ant[i].distance_sum;
+      pheromone[ant[i].route[j]][ant[i].route[j + 1]] += Q_VALUE / ant[i].distance_sum;
+      pheromone[ant[i].route[j + 1]][ant[i].route[j]] += Q_VALUE / ant[i].distance_sum;*/
     }
-    pheromone[ant[i].route[0]][ant[i].route[NUM_OF_CITIES - 1]] = (1 - VOLATILIZATION) * pheromone[ant[i].route[0]][ant[i].route[NUM_OF_CITIES - 1]];
+    tmp_pheromone[ant[i].route[0]][ant[i].route[NUM_OF_CITIES - 1]] += Q_VALUE / ant[i].distance_sum;
+    tmp_pheromone[ant[i].route[NUM_OF_CITIES - 1]][ant[i].route[0]] += Q_VALUE / ant[i].distance_sum;
+    /*pheromone[ant[i].route[0]][ant[i].route[NUM_OF_CITIES - 1]] = (1 - VOLATILIZATION) * pheromone[ant[i].route[0]][ant[i].route[NUM_OF_CITIES - 1]];
     pheromone[ant[i].route[NUM_OF_CITIES - 1]][ant[i].route[0]] = (1 - VOLATILIZATION) * pheromone[ant[i].route[NUM_OF_CITIES - 1]][ant[i].route[0]];
-    pheromone[ant[i].route[0]][ant[i].route[NUM_OF_CITIES - 1]] += 100 / ant[i].distance_sum;
-    pheromone[ant[i].route[NUM_OF_CITIES - 1]][ant[i].route[0]] += 100 / ant[i].distance_sum;
+    pheromone[ant[i].route[0]][ant[i].route[NUM_OF_CITIES - 1]] += Q_VALUE / ant[i].distance_sum;
+    pheromone[ant[i].route[NUM_OF_CITIES - 1]][ant[i].route[0]] += Q_VALUE / ant[i].distance_sum;*/
   }
+  //更新信息素，原信息素 * 衰减 + 蚂蚁走一圈生成的
+  for (int i = 0; i < NUM_OF_CITIES; i++) {
+    for (int j = 0; j < NUM_OF_CITIES; j++) {
+      pheromone[i][j] = (1 - VOLATILIZATION) * pheromone[i][j] + tmp_pheromone[i][j];
+      pheromone[j][i] = pheromone[i][j];
+    }
+  }
+
 }
+
+//
+////初始化蚂蚁，准备下一轮迭代
+//
 void InitAntsForNextIteration() {
   //初始化蚂蚁
      //初始化蚂蚁位置
-    memset(ant, 0, sizeof(ant));
-    for (int i = 0; i < POPULATION; i++) {
+  memset(ant, 0, sizeof(ant));
+  for (int i = 0; i < POPULATION; i++) {
     int randomstart = Random(0, NUM_OF_CITIES - 1);
     ant[i].route[0] = randomstart;
     ant[i].visited[randomstart] = 1;
     ant[i].currentsteps = 0;
     ant[i].distance_sum = 0;
   }
+}
 
+//
+//将文件数据存进内存
+//
+void GetTestDataFromFile() {
+  errno_t err;
+  FILE *fp;
+  err = fopen_s(&fp, "C:\\Users\\cky\\Documents\\GitHub\\TSPtestdata.txt", "r");
+  if (fp == NULL) {
+    printf("open file failed\n");
+    return;
+  }
+  fseek(fp, 0L, SEEK_END);/* 定位到文件末尾 */  //h.t.t.p.s://blog.csdn.net/jiayanhui2877/article/details/8222882
+  int flen = ftell(fp); /* 得到文件大小 */
+  char stringbuffer[100005] = { '\0' };
+  fseek(fp, 0L, SEEK_SET); /* 定位到文件开头 */
+  fread(stringbuffer, flen, 1, fp); /* 一次性读取全部文件内容 */
+  //TODO:: 缓冲区溢出了，文件里的\r\n，到了缓冲区里就剩个一字节的\n了
+  int notnumpos[100005];
+  int count = 0;
+  notnumpos[0] = -1;
+  for (int i = 0; i < flen; i++) {
+    if (stringbuffer[i] > '9' || stringbuffer[i] < '0') {
+      stringbuffer[i] = '\0';
+      notnumpos[count + 1] = i;
+      count++;
+    }
+  }
+  count = 0;
+  for (int i = 0; i < NUM_OF_CITIES; i++) {
+    sscanf_s(stringbuffer + notnumpos[2 * i] + 1, "%d", &cities[i].x);
+    sscanf_s(stringbuffer + notnumpos[2 * i + 1] + 1, "%d", &cities[i].y);
+  }
+  fclose(fp);
 }
 
 
 
 int main() {
+  GetTestDataFromFile();  //文件中读TSP数据
   int count = 0;
-  Init();
 
+  Init();
+  double mindistance = 0xFFFFFF;
   while (count <= MAX_ITERATION_TIMES) {
-    AllAntsMoveOneStep();
-    UpdatePheromoneAndInit();
-    double mindistance = 0xFFFFFF;
-    
+    AllAntsMoveAround();
+    UpdatePheromone();
+
+
     for (int i = 0; i < POPULATION; i++) {
       if (ant[i].distance_sum < mindistance) {
         mindistance = ant[i].distance_sum;
       }
     }
-    std::cout << mindistance << std::endl;
+    std::cout << count << ":  " << mindistance << std::endl;
 
     InitAntsForNextIteration();
     count++;
   }
+  return 0;
 }
--- 遗传算法GA/main.cpp
+++ 遗传算法GA/main.cpp
@@ -5,24 +5,63 @@
 #include <stdio.h>
 #include <math.h>
 #include <string>
+#include <fstream>
 
-#define SHOW_1_RESULT_PER_AMOUNT 1
-
-
-#define NUM_OF_CITIES 24
-#define POPULATION 50
-#define RANDOM_RANGE 100000
-#define CROSS_POSIBILITY 0.80
-#define MUTATE_POSIBILITY 0.00
-
-#define CROSS_LENGTH 5
-
-#define MAX_ITERATION_TIMES 1000000
+//要打开的tsp数据文件的路径，格式为每个点的坐标，具体格式见样例，可以从github下载数据文件来测试
+#define FILEPATH "C:\\Users\\cky\\Documents\\GitHub\\TSPtestdata.txt"
+//**************************************
+//
+//author: Duuang
+//https://github.com/Duuang/AI-course-exercise
+//
+//**************************************
 
 
+//**************************************
+//parameters
+//**************************************
 
-bool iffirsttime = true;
+#define POW_VALUE 10  //计算适应度时的指数大小
+#define SHOW_1_RESULT_PER_AMOUNT 100  //每多少次迭代显示一次结果
 
+#define NUM_OF_CITIES 48  //城市个数
+#define POPULATION 500     //种群数量
+#define RANDOM_RANGE 1000000    //生成随机数的最大范围，可以不用管
+#define CROSS_POSIBILITY 0.90  //交叉概率
+#define MUTATE_POSIBILITY 0.10  //变异概率
+
+#define CROSS_LENGTH 20  //采用固定长度段交叉时的交叉段长度
+
+#define MAX_ITERATION_TIMES 1000000  //最大迭代次数
+
+//城市坐标
+typedef struct {
+  int x, y;
+}City;
+
+//表示一个个体
+typedef struct {
+  int gene[NUM_OF_CITIES];  //编码方式为城市编号序列，如 {0, 1, 2, 3, 4}
+  double fitness;          //适应度
+  double ifselect_upper;  //严格大于轮盘赌的数
+  double ifselect_lower;  //小于或等于轮盘赌的数
+}TSP;
+
+//**************************************
+//global variables start here
+//**************************************
+
+bool iffirsttime = true;  //是否是第一次迭代（没啥用，用来在第一次的值不够坏时重新初始化）
+
+City cities[NUM_OF_CITIES];  //城市
+double distance[NUM_OF_CITIES][NUM_OF_CITIES];  //城市距离矩阵
+
+TSP father_generation[POPULATION];  //父代个体
+TSP next_generation[POPULATION];   //子代个体（实际上被用做缓冲区而已，主要保存在父代中）
+
+//
+//生成[x,y]范围的随机整数
+//
 int Random(int x, int y) {
   std::random_device rd;
   //std::default_random_engine a;
@@ -31,28 +70,10 @@ int Random(int x, int y) {
   return dist(rng);
 }
 
-
-typedef struct {
-  int x, y;
-}City;
-
-//int gene[POPULATION][NUM_OF_CITIES];
-typedef struct {
-  int gene[NUM_OF_CITIES];
-  double fitness;
-  double ifselect_upper;  //严格大于
-  double ifselect_lower;  //小于或等于轮盘赌的数
-}TSP;
-
-City cities[NUM_OF_CITIES] = { {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6}, {1, 7}
-, {2, 7}, {3, 7}, {4, 7}, {5, 7}, {6, 7}, {7, 7}, {7, 6}, {7, 5}, {7, 4}, {7, 3}, {7, 2}, {7, 1}
-, {6, 1}, {5, 1}, {4, 1}, {3, 1}, {2, 1} };
-double distance[NUM_OF_CITIES][NUM_OF_CITIES];
-
-TSP father_generation[POPULATION];
-TSP next_generation[POPULATION];
-
-void CitiesInit() {
+//
+//初始化距离矩阵，和个体的基因
+//
+void Init() {
   //初始化距离矩阵，下标从0开始，到n-1
   for (int i = 0; i < NUM_OF_CITIES; i++) {
     for (int j = 0; j < NUM_OF_CITIES; j++) {
@@ -76,10 +97,12 @@ void CitiesInit() {
           break;
       }
     }
-
   }
 }
 
+//
+//计算群体中每个个体的适应度，是用的默认公式加了指数
+//
 void CalcFitness(TSP *individual) {
   double fitness;
   for (int i = 0; i < POPULATION; i++) {
@@ -88,13 +111,17 @@ void CalcFitness(TSP *individual) {
       fitness += distance[(individual + i)->gene[j]][(individual + i)->gene[j + 1]];
     }
     fitness += distance[(individual + i)->gene[0]][(individual + i)->gene[NUM_OF_CITIES - 1]];
-    fitness = pow(fitness, 3);
+    //！！！因为。。觉得收敛不够快，就加了个指数上去，普通遗传算法是指数为1的情况
+    fitness = pow(fitness, POW_VALUE);
     fitness = 1.0 / fitness;
     father_generation[i].fitness = fitness;
-
   }
 }
 
+//
+//选择，从父代数组中选择，保存到子代数组
+//用轮盘赌方式，概率性选择
+//
 void Select() {
   double fitness_sum = 0;
   for (int i = 0; i < POPULATION; i++) {
@@ -111,6 +138,7 @@ void Select() {
   for (int i = 0; i < POPULATION; i++) {
     int randomnum = Random(0, RANDOM_RANGE);
     for (int j = 0; j < POPULATION; j++) {
+      //如果轮盘赌选中了，则把这个个体加到子代中，继续轮盘赌选下一个
       if (father_generation[j].ifselect_lower <= randomnum && father_generation[j].ifselect_upper > randomnum) {
         memcpy(next_generation + i, father_generation + j, sizeof(father_generation[j]));
         break;
@@ -120,17 +148,24 @@ void Select() {
       }
     }
   }
-  //父亲复制为子代值，下一步交叉在父代数组中进行
-  memcpy(father_generation, next_generation, sizeof(next_generation));
 }
 
+//
+//交叉函数1，失败的交叉函数。。用的交替位置交叉法（Alternating Position Crossover，APX）
+//事实证明这个交叉方法就是逗比，因为根本不保留整段基因啊。。跟完全随机有啥区别么？
+//程序中调用的是下面的Cross2()函数
+//
 void Cross() {
+  //交叉概率，判断是否交叉
   double cross_possibility = CROSS_POSIBILITY;
   for (int i = 0; i < POPULATION; i++) {
     int randomnum = Random(0, RANDOM_RANGE);
     if (randomnum > cross_possibility * RANDOM_RANGE) {
       continue;
       //交叉，用的交替位置交叉法（Alternating Position Crossover，APX）
+      //大概意思是，基因1：a1 a2 a3 a4 a5
+      //           基因2：b1 b2 b3 b4 b5
+      //然后按照a1->b1->a2->b2->a3......的顺序，依次把一个基因选出来组成DNA，如果遇到重复的则跳过，看下一个
     } else {
       int father1 = Random(0, POPULATION - 1);
       int father2 = Random(0, POPULATION - 1);
@@ -156,12 +191,17 @@ void Cross() {
             break;
         }
       }
-
     }
   }
 }
 
+//
+//交叉函数2，吸取了教训，这回用的是整段基因的交叉。。
+//交叉长度已经给定，随机选择交叉起点，个体1的基因和个体2的基因在这段上交换，
+//然后，其他基因就按照个体1的填，如果遇到冲突，则选择填入一个最小的不冲突的城市
+//
 void Cross2() {
+  //交叉概率
   double cross_possibility = CROSS_POSIBILITY;
   for (int i = 0; i < POPULATION; i++) {
     int randomnum = Random(0, RANDOM_RANGE);
@@ -173,15 +213,16 @@ void Cross2() {
       int father2 = Random(0, POPULATION - 1);
       //指定一个固定长度的一段，起始位置随机，来进行两点间交叉
       int exist[NUM_OF_CITIES] = { 0 };
+      //pos:起始位置
       int pos = Random(1, NUM_OF_CITIES - 1 - (CROSS_LENGTH - 1));
-      
+
+      //交换指定的那段基因
       for (int j = pos; j < pos + CROSS_LENGTH; j++) {
         int tmp = next_generation[father2].gene[j];
-        next_generation[father2].gene[j] = next_generation[father1].gene[j];
-        next_generation[father1].gene[j] = tmp;
         father_generation[i].gene[j] = tmp;
         exist[tmp] = 1;
       }
+      //其他的基因，按照能填就填，冲突就找一个不冲突的，这么填完
       for (int j = 1; j < pos; j++) {
         if (exist[next_generation[father1].gene[j]] == 0) {
           father_generation[i].gene[j] = next_generation[father1].gene[j];
@@ -210,42 +251,45 @@ void Cross2() {
           }
         }
       }
-
-
-        /*while (true) {
-          int ifallnoneexist = true;
-          for (int k = pos; k < pos + CROSS_LENGTH; k++) {
-            if (exist[next_generation[father1].gene[k]] != 0) {
-              ifallnoneexist = false;
-              int tmp = next_generation[father2].gene[k];
-              next_generation[father2].gene[k] = next_generation[father1].gene[k];
-              next_generation[father1].gene[k] = tmp;
-              break;
-            }
-          }
-          if (ifallnoneexist)
+      /*while (true) {
+        int ifallnoneexist = true;
+        for (int k = pos; k < pos + CROSS_LENGTH; k++) {
+          if (exist[next_generation[father1].gene[k]] != 0) {
+            ifallnoneexist = false;
+            int tmp = next_generation[father2].gene[k];
+            next_generation[father2].gene[k] = next_generation[father1].gene[k];
+            next_generation[father1].gene[k] = tmp;
             break;
-        }*/
-        /*if (exist[next_generation[father2].gene[j]] == 0) {
-          father_generation[i].gene[j] = next_generation[father2].gene[j];
-          exist[next_generation[father2].gene[j]] = 1;
-        }*/
-        
-      
+          }
+        }
+        if (ifallnoneexist)
+          break;
+      }*/
+      /*if (exist[next_generation[father2].gene[j]] == 0) {
+        father_generation[i].gene[j] = next_generation[father2].gene[j];
+        exist[next_generation[father2].gene[j]] = 1;
+      }*/
 
     }
   }
 }
 
+//
+//突变函数
+//若在突变概率中，则随机选两个位置，交换即可
+//
 void Mutate() {
+  //突变概率，判断是否进行突变
   double mutate_possibility = MUTATE_POSIBILITY;
   for (int i = 0; i < POPULATION; i++) {
     int randomnum = Random(0, RANDOM_RANGE);
     if (randomnum > mutate_possibility * RANDOM_RANGE) {
       continue;
     } else {
+      //随机两个位置
       int pos1 = Random(1, NUM_OF_CITIES - 1);
       int pos2 = Random(1, NUM_OF_CITIES - 1);
+      //交换
       int tmp = father_generation[i].gene[pos1];
       father_generation[i].gene[pos1] = father_generation[i].gene[pos2];
       father_generation[i].gene[pos2] = tmp;
@@ -253,10 +297,46 @@ void Mutate() {
   }
 }
 
+//
+//将文件数据存进内存
+//
+void GetTestDataFromFile() {
+  errno_t err;
+  FILE *fp;
+  err = fopen_s(&fp, "C:\\Users\\cky\\Documents\\GitHub\\TSPtestdata.txt", "r");
+  if (fp == NULL) {
+    printf("open file failed\n");
+    return;
+  }
+  fseek(fp, 0L, SEEK_END);/* 定位到文件末尾 */  //h.t.t.p.s://blog.csdn.net/jiayanhui2877/article/details/8222882
+  int flen = ftell(fp); /* 得到文件大小 */
+  char stringbuffer[100005] = { '\0' };
+  fseek(fp, 0L, SEEK_SET); /* 定位到文件开头 */
+  fread(stringbuffer, flen, 1, fp); /* 一次性读取全部文件内容 */
+  //TODO:: 缓冲区溢出了，文件里的\r\n，到了缓冲区里就剩个一字节的\n了
+  int notnumpos[100005];
+  int count = 0;
+  notnumpos[0] = -1;
+  for (int i = 0; i < flen; i++) {
+    if (stringbuffer[i] > '9' || stringbuffer[i] < '0') {
+      stringbuffer[i] = '\0';
+      notnumpos[count + 1] = i;
+      count++;
+    }
+  }
+  count = 0;
+  for (int i = 0; i < NUM_OF_CITIES; i++) {
+    sscanf_s(stringbuffer + notnumpos[2 * i] + 1, "%d", &cities[i].x);
+    sscanf_s(stringbuffer + notnumpos[2 * i + 1] + 1, "%d", &cities[i].y);
+  }
+  fclose(fp);
+}
 
 
 int main() {
-  CitiesInit();
+  GetTestDataFromFile();
+
+  Init();
 
   int count = 0;
   while (count < MAX_ITERATION_TIMES) {
@@ -264,6 +344,7 @@ int main() {
       CalcFitness(father_generation);
       if (!iffirsttime) {
         int lastmaxpos = 0;
+        //count为设定值的倍数时，才输出到控制台
         if (count % SHOW_1_RESULT_PER_AMOUNT == 0) {
           double mindistance = 0;
           for (int i = 0; i < POPULATION; i++) {
@@ -271,18 +352,15 @@ int main() {
               mindistance = father_generation[i].fitness;
               lastmaxpos = i;
             }
-            }
-          
-          
-          
+          }
           mindistance = 1 / mindistance;
-          mindistance = pow(mindistance, 1.0 / 3);
-            std::cout << mindistance << std::endl;
+          mindistance = pow(mindistance, 1.0 / POW_VALUE);
+
+          std::cout << count << ":  " << mindistance << std::endl;
           if (mindistance < 23.9) {
             printf("error!!!: %d\n", lastmaxpos);
             system("pause");
           }
-          
         }
         break;
 
@@ -294,14 +372,14 @@ int main() {
           }
         }
         mindistance = 1 / mindistance;
-         mindistance = pow(mindistance, 1.0 / 3);
+        mindistance = pow(mindistance, 1.0 / POW_VALUE);
         if (mindistance > 50) {
-          
-          std::cout << mindistance << std::endl;
+
+          std::cout << "initial: " << mindistance << std::endl;
           iffirsttime = false;
           break;
         } else {
-          CitiesInit();
+          Init();
         }
       }
     }
@@ -310,7 +388,6 @@ int main() {
     Select();
     Cross2();
     Mutate();
-
 
   }
   //std::cout << mindistance << std::endl;
@@ -327,4 +404,5 @@ int main() {
   Sleep(1000);
   printf("iteration stopped\n");
   system("pause");
+  return 0;
 }
